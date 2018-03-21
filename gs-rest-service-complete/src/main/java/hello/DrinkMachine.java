bf9c3544b10b5d80000295053d6895e14c36dad2
syntel <syntel@PGX440G3-2815WS.syntelorg.com>
2018-03-21 16:42:46 -0400

    bug in drinkmachine need to fix

--- gs-rest-service-complete/src/main/java/hello/DrinkMachine.java
+++ gs-rest-service-complete/src/main/java/hello/DrinkMachine.java
@@ -1,4 +1,56 @@
 package hello;
 
 public class DrinkMachine {
+  private   int[] cokeCannedDrink = new int[6];
+  private    int[] fantaCannedDrink = new int[6];
+  private   int[] spriteCannedDrink = new int[6];
+  private   int[] schweppsBottledDrink = new int[5];
+  private   int[] pepsiBottledDrink = new int[5];
+
+
+    private  int sum = 0;
+
+   private int numOfProducts;
+
+    public void addProduct(){
+        for(int i = 0; i < 5;i++){
+            if(i >=2){
+                for(int j  = 0; j < 6; j++) {
+                    System.out.println("in canned");
+                    cokeCannedDrink[j] = 12;
+                    spriteCannedDrink[j] = 12;
+                    fantaCannedDrink[j] = 12;
+                }
+            }else{
+                for(int j = 0; j < 5;j++) {
+                    System.out.println("in bottled");
+                    pepsiBottledDrink[i] = 8;
+                    schweppsBottledDrink[i] = 8;
+                }
+            }
+        }
+    }
+    public int getAmountOfCannedCokes(){
+        return getAmountOfProducts(cokeCannedDrink);
+    }public int getAmountOfCannedFanta(){
+        return getAmountOfProducts(fantaCannedDrink);
+    }public int getAmountOfCannedSprite(){
+        return getAmountOfProducts(spriteCannedDrink);
+    }public int getAmountOfBottledPepsi(){
+        return getAmountOfProducts(pepsiBottledDrink);
+    }public int getAmountOfBottledSchwepps(){
+        return getAmountOfProducts(schweppsBottledDrink);
+    }
+
+
+    public int getAmountOfProducts(int[] arr){
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+            sum+=arr[i];
+        }
+        return sum;
+
+    }
 }
+
