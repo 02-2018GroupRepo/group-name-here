0d605c4a1441ff8441af8288502d8516fafcdfe0
syntel <syntel@PHX440G3-281607.syntelorg.com>
2018-03-22 11:55:24 -0400

    returns difference in coins

--- gs-rest-service-complete/src/test/groovy/CoinManager.java
+++ gs-rest-service-complete/src/test/groovy/CoinManager.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CoinManager {
 
     private ArrayList<Coins> coinList = new ArrayList<Coins>();
+    private HashMap<Double, Integer> coinLog;
 
 
+    //Validates coins
     public void addCoin(Coins c){
         if(c.getValue() != -1){
             this.coinList.add(c);
@@ -14,10 +17,12 @@ public class CoinManager {
         }
     }
 
+    //
     public int howMuchCoins(){
         return this.coinList.size();
     }
 
+    //Get total amount of valid coins
     public double totalAmount(){
         double total = 0;
         for( Coins c : this.coinList){
@@ -25,6 +30,49 @@ public class CoinManager {
         }
         return total;
     }
+
+    //Coins that are currently in the machine
+    public CoinManager(){
+        coinLog = new HashMap<>();
+        coinLog.put(Coins.getQUARTER(), 15);
+        coinLog.put(Coins.getDIME(), 15);
+        coinLog.put(Coins.getNICKEL(), 15);
+        //resetSession();
+
+    }
+
+    //return change
+    public void returnCustomerChange(double difference){
+        do {
+            if (difference % Coins.getQUARTER() == 0) {
+                coinLog.put(Coins.getQUARTER(), coinLog.get(Coins.getQUARTER()) - 1);
+                difference -= Coins.getQUARTER();
+            } else if (difference % Coins.getDIME() == 0) {
+                coinLog.put(Coins.getDIME(), coinLog.get(Coins.getDIME()) - 1);
+                difference -= Coins.getDIME();
+            } else if (difference % Coins.getNICKEL() == 0) {
+                coinLog.put(Coins.getNICKEL(), coinLog.get(Coins.getNICKEL()) - 1);
+                difference -= Coins.getNICKEL();
+            }
+
+        }while (difference > 0);
+
+
+    }
+
+
+
+
+
+
+
+//    public void resetSession(){
+//        int resetNickel = 0;
+//        int resetDime = 0;
+//        int resetQuarter = 0;
+//
+//
+//    }
 
 
 }
